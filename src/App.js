938d859d49c4c82da7960d93cba24cc5c8752a53
薛时鸣 B30974 <xueshiming01@corp.netease.com>
2023-02-15 17:13:49 +0800

    feat: 更改脚本

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components'
-import * as icons from './icons'
-import Header from './components/Header'
-import IconWrapper from './components/IconWrapper'
+import React from "react";
+import styled from "styled-components";
+import * as icons from "./icons";
+import Header from "./components/Header";
+import IconWrapper from "./components/IconWrapper";
 
 const Container = styled.ul`
   display: grid;
@@ -12,42 +12,41 @@ const Container = styled.ul`
   margin: 0;
   padding: 0 0 50px 0;
   list-style: none;
-`
+`;
 
 class List extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Container>
-          {
-            Object.keys(icons)
-              .map((key, index) => {
-                const Icon = icons[key]
-                return <li key={index}>
-                  <IconWrapper>
-                    <Icon/>
-                    <span onDoubleClick={() => handleCopyIcon(key)}>{key}</span>
-                  </IconWrapper>
-                </li>
-              })
-          }
+          {Object.keys(icons).map((key, index) => {
+            const Icon = icons[key];
+            return (
+              <li key={index}>
+                <IconWrapper>
+                  <Icon />
+                  <span onDoubleClick={() => handleCopyIcon(key)}>{key}</span>
+                </IconWrapper>
+              </li>
+            );
+          })}
         </Container>
       </div>
-    )
+    );
   }
 }
 
 // copy icon
 function handleCopyIcon(str) {
-  const el = document.createElement('textarea');
+  const el = document.createElement("textarea");
   el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
   document.body.appendChild(el);
   el.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(el);
 }
 
